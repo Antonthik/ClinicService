1c81a68f1a8566dcc29364b9a01e7a87c914e2c1
antonchik <ganseanderson@gmail.com>
2022-12-08 23:21:45 +0300

    les 1.Приенение  микросервиса ASP.Core Web-API.Интеграция сервиса с системой удаленного вызова gRPC.Создание клиента gRPC  в ASP.Net Core

---
+++ ClinicService.Data/Pet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicService.Data
+{
+    [Table("Pets")]
+    public class Pet
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PetId { get; set; }
+
+        [ForeignKey(nameof(Client))]//ключевое поле
+        public int ClientId { get; set; }
+
+        [Column]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Column]
+        public DateTime Birthday { get; set; }
+
+        public virtual Client Client { get; set; }//ссылка на клиента только один у животного
+
+        [InverseProperty(nameof(Consultation.Pet))]
+        public virtual ICollection<Consultation> Consultations { get; set; } = new HashSet<Consultation>();//у животного много клнсультаций
+
+    }
+}
